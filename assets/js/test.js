654033186fde8f20450ed9b86de74a7f34317b5b
Elena-Pakhmurskaia <elenapakhmurskaia@gmail.com>
2020-10-03 17:35:25 +0000

    added waypoint JS and placed placeholder marker on direction page

--- assets/js/test.js
+++ assets/js/test.js
@@ -7,28 +7,12 @@ google.maps.event.addDomListener(window, 'load', function () {
     directionsDisplay = new google.maps.DirectionsRenderer({ 'draggable': true });
 });
  
-
-
 function GetRoute() {
     var europe = new google.maps.LatLng(49.81888, 3.30115);
     var mapOptions = {
         zoom: 5,
         center: europe
     };
-
-    var waypts = [];
-    var checkboxArray = document.getElementById("txtWaypoint");
-
-        for (let i = 0; i < checkboxArray.length; i++) {
-          if (checkboxArray.options[i].selected) {
-            waypts.push({
-              location: checkboxArray[i].value,
-              stopover: true,
-            });
-          }
-        }
-
-
 
     map = new google.maps.Map(document.getElementById('dvMap'), mapOptions);
     directionsDisplay.setMap(map);
@@ -37,12 +21,10 @@ function GetRoute() {
     //*********DIRECTIONS AND ROUTE**********************//
     source = document.getElementById("txtSource").value;
     destination = document.getElementById("txtDestination").value;
-    waypoint = document.getElementById("txtWaypoint").value
  
     var request = {
         origin: source,
         destination: destination,
-        waypoints: waypts,
         travelMode: google.maps.TravelMode.DRIVING
     };
     directionsService.route(request, function (response, status) {
@@ -74,16 +56,4 @@ function GetRoute() {
             alert("Unable to find the route.");
         }
     });
-
-        var marker = new google.maps.Marker({
-      
-        position: {lat: 50.694284, lng: 5.626468},
-		iconImage: "/assets/images/shell logo.png",
-		content: '<h5>Antwerp Shell Station</h5><br><h6>Pay here with your EuroShell Card</h6>',
-        });
-
-        marker.setMap(map);
 }
-
-
-   
